0af52053bf5be96b2c84fc14fbc424185297fce7
Pius N Fernandez <piusfernandez@hotmail.com>
2018-03-11 15:44:45 -0400

    spring5 from springguru udemy

---
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -0,0 +1,99 @@
+package guru.springframework.spring5webapp.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import org.hibernate.annotations.ManyToAny;
+
+@Entity
+public class Author {
+
+	/*
+	 * Leakage between (ORM) relation and object oriented paradigm ORM need a unique
+	 * id
+	 */
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String firstName;
+	private String lastName;
+	
+	@ManyToMany(mappedBy="authors")
+	private Set<Book> books = new HashSet<>();
+
+	public Author() {
+	}
+
+	public Author(String firstName, String lastName, Set<Book> books) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.books = books;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName
+	 *            the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName
+	 *            the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the books
+	 */
+	public Set<Book> getBooks() {
+		return books;
+	}
+
+	/**
+	 * @param books
+	 *            the books to set
+	 */
+	public void setBooks(Set<Book> books) {
+		this.books = books;
+	}
+
+}
